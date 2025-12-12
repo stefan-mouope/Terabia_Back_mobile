7c197746310d33827382abefdb8fcdb675ec20e2
LaVue <sandjonyves@gmail.com>
2025-12-12 20:54:37 +0100

    deply

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,9 @@
-// module.exports = {
-//   DB_USER: process.env.DB_USER || 'your_db_user',
-//   DB_HOST: process.env.DB_HOST || 'localhost',
-//   DB_DATABASE: process.env.DB_DATABASE || 'your_db_name',
-//   DB_PASSWORD: process.env.DB_PASSWORD || 'your_db_password',
-//   DB_PORT: process.env.DB_PORT || 5432,
-//   PORT: process.env.PORT || 3000,
-//   JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
-// };
+module.exports = {
+  DB_USER: process.env.DB_USER || 'your_db_user',
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_DATABASE: process.env.DB_DATABASE || 'your_db_name',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'your_db_password',
+  DB_PORT: process.env.DB_PORT || 5432,
+  PORT: process.env.PORT || 3000,
+  JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
+};
--- terabia2/constants/api.ts
+++ terabia2/constants/api.ts
@@ -8,9 +8,9 @@ import { Platform } from 'react-native';
 // Ton IP actuelle trouvée avec ifconfig (interface wlp3s0)
 const SERVER_IP = '192.168.0.109'; 
 
-export const API_BASE_URL = __DEV__ 
-  ? `http://${Platform.OS === 'android' ? SERVER_IP : 'localhost'}:3000/api`
-  : 'https://ton-api-prod.com/api';
+// export const API_BASE_URL = __DEV__ 
+//   ? `http://${Platform.OS === 'android' ? SERVER_IP : 'localhost'}:3000/api`
+//   : 'https://ton-api-prod.com/api';
 
 
-// export const API_BASE_URL = 'https://terabia-back-mobile.onrender.com/api'
+export const API_BASE_URL = 'https://terabia-back-mobile.onrender.com/api'
